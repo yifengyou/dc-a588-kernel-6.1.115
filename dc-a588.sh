6990a20860ca028490e7563eb17ac30c83518b98
yifengyou <842056007@qq.com>
2025-07-08 13:43:49 +0800

    add dc-a588 dts suport. dts from qq group.
    Signed-off-by: yifengyou <842056007@qq.com>

--- dc-a588.sh
+++ dc-a588.sh
@@ -49,10 +49,6 @@ rm -rf output
 mkdir -p output/modules
 cp arch/arm64/boot/Image output
 
-find . -name "*.ko" |xargs -i /bin/cp -a {} output/modules/
-ls -alh output/modules/
-md5sum  output/modules/*.ko
-
 cd output
 cp -a ../tools/boot/* .
 cp -a ../arch/arm64/boot/dts/rockchip/rk3588-dc-a588.dtb .
